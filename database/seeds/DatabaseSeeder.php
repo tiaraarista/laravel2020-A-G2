8210765cb88a40c0e3b0e7d7f73ff42b4c809541
tiaraarista <tiaraarista99@gmail.com>
2020-04-07 22:38:41 +0700

    Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(AdminSeeder::class);
+        $this->call(UsersSeeder::class);
+        $this->call(CategoriesSeeder::class);
     }
 }
---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+
+        for($i = 1; $i <= 99; $i++){
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email'=> $faker->safeEmail,
+                'email_verified_at' => now(),
+                'password' => bcrypt(12345678),
+                'role' => 'staff', 
+            ]);
+        }
+    }
+}
