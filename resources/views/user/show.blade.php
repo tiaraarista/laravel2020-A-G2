6242bc1ac9a5c2e0c331231388791971ce34c70f
NurAviatunJanah <nuraviajanah14@gmail.com>
2020-04-08 15:13:35 +0700

    user show

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,21 +1,7 @@
 @extends('layouts.master')
 
 @section('top')
-    <!-- DataTables -->
-    <link rel="stylesheet" href="{{ asset('template/vendor/datatables/dataTables.bootstrap4.min.css') }}">
 
-    <!-- Custom styles for this page -->
-    <link href="{{ asset('template/vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
-
-    <!-- Custom scripts for all pages-->
-    <script src="{{ asset('template/js/sb-admin-2.min.js') }}"></script>
-
-    <!-- Page level plugins -->
-    <script src="{{ asset('template/vendor/datatables/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('template/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="{{ asset('template/js/demo/datatables-demo.js') }}"></script>
 @endsection
 
 @section('content')
@@ -29,24 +15,23 @@
 <div class="card shadow mb-4">
   <div class="card-header py-3">
     <!-- <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6> -->
+    <a href="{{ route('user.index') }}" class="btn btn-sm btn-primary float-right"><i class="fas fa-fw fa-chevron-left"></i> Back</a> 
   </div>
   <div class="card-body">
     <div class="table-responsive">
-      <table  id="dataTable" class="table table-bordered" width="100%" cellspacing="0">
-        <thead>
+      <table class="table table-bordered" width="100%" cellspacing="0">
+        <thead class="thead-light">
           <tr>
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
-            <th><center>Action</center></th>
           </tr>
         </thead>
-        <tfoot>
+        <tfoot class="thead-light">
           <tr>
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
-            <th><center>Action</center></th>
           </tr>
         </tfoot>
         <tbody>
@@ -55,17 +40,6 @@
                 <td>{{ $usr->name }}</td>
                 <td>{{ $usr->email }}</td>
                 <td>{{ $usr->role }}</td>
-                <td><center><a href="{{route('user.show', $usr->id)}}" class="btn btn-primary"><i class="fas fa-eye"></i> Lihat</a> 
-                <a href="{{route('user.edit', $usr->id)}}" class="btn btn-primary"><i class="fas fa-user-edit"></i> Edit</a><br>
-                <!-- <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#hapus"><i class="fas fa-trash-alt"></i>  
-                Hapus</button> -->
-                <form action="{{route('user.destroy', $usr->id)}}" method="post">
-                    @csrf
-                    <input type="hidden" name="_method" value="DELETE">
-                    <input type="hidden" value="{{$usr->id}}" name="name">
-                    <input type="submit" value="Hapus" onclick="return alert('Apakah anda yakin?')">
-                </form>
-                </center>
             </tr>
           @endforeach
         </tbody>
